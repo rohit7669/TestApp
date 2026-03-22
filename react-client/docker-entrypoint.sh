24dde031ab9731f307f61ec992b1f9e8d6753ae1
rohit7669 <srivastava.rohit33@gmail.com>
2026-03-22 17:35:24 +0530

    second commit

--- react-client/docker-entrypoint.sh
+++ react-client/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Dynamically create env.js
+# Dynamically create env.js based on environment variable
 cat <<EOF > /usr/share/nginx/html/env.js
 window._env_ = {
   REACT_APP_API_URL: "${REACT_APP_API_URL:-http://backend:8080/api}"
