494883d185ddf3433fe537a846ef069d0a0d11f9
rohit7669 <srivastava.rohit33@gmail.com>
2026-03-25 15:29:24 +0530

    added k8s folder

--- react-client/public/env.js
+++ react-client/public/env.js
@@ -1,4 +1,4 @@
 // frontend/public/env.js
 window._env_ = {
-  REACT_APP_API_URL: "http://backend:8080/api" // default fallback
+  REACT_APP_API_URL: "http://13.201.6.246:30081/api" // default fallback
 };
