2929e6cf1977a2ff652e548356352840539ba3a3
rohit7669 <srivastava.rohit33@gmail.com>
2026-03-26 15:42:59 +0530

    Added ingress file

--- react-client/docker-entrypoint.sh
+++ react-client/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-# Dynamically create env.js based on environment variable
+# Dynamically create env.js based on environment variable at container start
 cat <<EOF > /usr/share/nginx/html/env.js
 window._env_ = {
   REACT_APP_API_URL: "${REACT_APP_API_URL:-/api}"
 };
 EOF
 
+# Start nginx
 exec "$@"
